f37db3dbd376caa901aa167f2cef527423a853ae
BenFeder <benjamintfeder@gmail.com>
2021-07-23 14:09:44 -0400

    Added ListAdapter and viewholder for total amount, and grocery list

--- app/src/main/java/com/benfeder/shoppinglist/ui/view/ListAdapter.kt
+++ app/src/main/java/com/benfeder/shoppinglist/ui/view/ListAdapter.kt
@@ -5,18 +5,18 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.benfeder.shoppinglist.data.local.Grocery
-import com.benfeder.shoppinglist.databinding.FragmentShoppingBinding
-import com.benfeder.shoppinglist.databinding.ShoppingItemBinding
+import com.benfeder.shoppinglist.databinding.FragmentListBinding
+import com.benfeder.shoppinglist.databinding.ListItemBinding
 import com.benfeder.shoppinglist.ui.viewmodel.ShoppingViewModel
 
-class ShoppingAdapter(
+class ListAdapter (
     private val listener: (grocery: Grocery) -> Unit
 ) : RecyclerView.Adapter<ShoppingAdapter.ShoppingViewHolder>() {
     private var groceryList = listOf<Grocery>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
-        return ShoppingViewHolder(
-            ShoppingItemBinding.inflate(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        return ListViewHolder(
+            ListItemBinding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
                 false
@@ -26,23 +26,30 @@ class ShoppingAdapter(
         }
     }
 
-    override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         holder.bind(groceryList[position])
     }
 
     override fun getItemCount() = groceryList.size
 
-    fun loadData(groceries : List<Grocery>) {
+    fun loadData(groceries: List<Grocery>) {
         groceryList = groceries
         notifyDataSetChanged()
     }
 
-    class ShoppingViewHolder(private val binding: ShoppingItemBinding) :
+    class ListViewHolder(private val binding: ListItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(grocery: Grocery) = with(binding) {
-            priceTv.text = "$" + grocery.price.toString()
-            groceryTv.text = grocery.grocery
+            listPriceTv.text = "$" + grocery.price.toString()
+            listGroceryTv.text = grocery.grocery
         }
     }
 
+    class TotalViewHolder(private val binding: FragmentListBinding) :
+        TextView.ViewHolder(binding.root) {
+        fun bind(price: ShoppingViewModel) = with(binding) {
+            val totalAmount = price.totalAmount
+            totalTv.text = "$" + totalAmount.toString()
+        }
+    }
 }
